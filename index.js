2b720eb3040291b375ff17a072dafcc9cf395554
renangarrio-dio <renangarrio@outlook.com.br>
2024-01-08 12:17:49 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let heroi = "homem de ferro";
+let xp = 6002
+let nivel = ""
+
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp > 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp > 2001 && xp <= 5000) {
+    nivel = "prata";
+} else if (xp > 6001 && xp <= 7000) {
+    nivel = "ouro";
+} else if (xp > 7001 && xp <= 8000) {
+    nivel = "platina";
+} else if (xp > 8001 && xp <= 9000) {
+    nivel = "ascendente";
+} else if (xp > 9001 && xp <= 10000) {
+    nivel = "imortal";
+} else if (xp >= 10001) {
+    nivel = "radiante";
+}
+
+console.log ("O nome do heroi " +heroi+ " esta no nivel de " + nivel);
+
